1b7e2bf99bd6777e9dbd58d99be6c81aec9c2bdc
parsaa74 <133056472+parsaa74@users.noreply.github.com>
2025-01-23 16:32:08 +0000

    Deploying to gh-pages from @ parsaa74/german-art-schools@f9c653f777a759ce6756df55512f833ac942df35 🚀

---
+++ _next/static/chunks/app/error-babb2121e3c5db8c.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[601],{1682:function(e,r,n){Promise.resolve().then(n.bind(n,6352))},6352:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return s}});var t=n(7437),o=n(2265);function s(e){let{error:r,reset:n}=e;return(0,o.useEffect)(()=>{console.error(r)},[r]),(0,t.jsxs)("div",{className:"flex min-h-screen flex-col items-center justify-center",children:[(0,t.jsx)("h2",{className:"text-2xl font-bold mb-4",children:"Something went wrong!"}),(0,t.jsx)("button",{className:"px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600",onClick:()=>n(),children:"Try again"})]})}},622:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,u=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function i(e,r,n){var t,s={},i=null,l=null;for(t in void 0!==n&&(i=""+n),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(l=r.ref),r)c.call(r,t)&&!f.hasOwnProperty(t)&&(s[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===s[t]&&(s[t]=r[t]);return{$$typeof:o,type:e,key:i,ref:l,props:s,_owner:u.current}}r.Fragment=s,r.jsx=i,r.jsxs=i},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=1682)}),_N_E=e.O()}]);
